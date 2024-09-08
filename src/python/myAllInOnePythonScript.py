ae3dc6c5d03cc2b710a7c9f88b834f0bff9f8a65
onkar-r <131953102+onkar-r@users.noreply.github.com>
2024-09-08 17:18:33 +0530

    reversed the string

--- src/python/myAllInOnePythonScript.py
+++ src/python/myAllInOnePythonScript.py
@@ -54,4 +54,4 @@ while (True):
 
 print("String slicing")
 mystr = "HelloWorld"
-print(f"Full string = {mystr}, Just Hello part = {mystr[:5]}") # Syntax: str[start pos: end pos : step]
+print(f"Full string = {mystr}, Just Hello part = {mystr[:5]}, Reverse = {mystr[::-1]}") # Syntax: str[start pos: end pos : step]
